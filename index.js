5ac0939006a5f9e267b4078c9e5caba68423c891
anaghcodes <anaghcodes@gmail.com>
2025-09-05 22:10:03 +0530

    Project 3

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const { url } = require("inspector");
 const app = express();
+
+let urls = new Object();
+let counter = 1;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post("/api/shorturl", function (req, res) {
+  const original_url = req.body.url;
+
+  const hostname = new URL(original_url).hostname;
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    }
+    urls[counter] = original_url;
+
+    res.json({ original_url: original_url, short_url: counter });
+
+    counter++;
+  });
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:code", (req, res) => {
+  const code = Number(req.params.code);
+  if (urls[code]) {
+    res.redirect(urls[code]);
+  } else {
+    res.json({ error: "Invalid url" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
